ca836406d2c669e396e5ad539a347beeb8b379fc
Edsson Madrigal <akairu@hotmail.com>
2023-09-24 18:22:55 -0600

    function calling part 3

--- server_weather.js
+++ server_weather.js
@@ -82,6 +82,55 @@ async function getWeather(parsed_function_arguments) {
   }
 }
 
+//runCompletion2
+async function runCompletion2(prompt, function_arguments, weatherObject) {
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "user", content: prompt },
+      {
+        role: "assistant",
+        content: null,
+        function_call: {
+          name: "get_current_weather",
+          arguments: function_arguments,
+        },
+      },
+      {
+        role: "function",
+        name: "get_current_weather",
+        content: JSON.stringify(weatherObject),
+      },
+    ],
+    functions: [
+      {
+        name: "get_current_weather",
+        description: "Get the current weather in a given location",
+        parameters: {
+          type: "object",
+          properties: {
+            location: {
+              type: "string",
+              description: "The city and state, e.g. San Francisco, CA",
+            },
+            unit: {
+              type: "string",
+              enum: ["celsius", "fahrenheit"],
+            },
+          },
+          required: ["location"],
+        },
+      },
+    ],
+    temperature: 1,
+    max_tokens: 50,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  return response;
+}
+
 app.post("/api/chatgpt", async (req, res) => {
   try {
     const { text } = req.body;
@@ -112,6 +161,17 @@ app.post("/api/chatgpt", async (req, res) => {
       //temperature, unit & description
       const weatherObject = await getWeather(parsed_function_arguments);
       console.log(weatherObject);
+
+      //request 3
+      const response = await runCompletion2(
+        text,
+        function_arguments,
+        weatherObject
+      );
+
+      //send response to react application
+      console.log(response);
+      res.json(response);
     }
   } catch (error) {
     if (error.response) {
