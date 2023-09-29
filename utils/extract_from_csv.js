936ffb096878d3bf72b81ce1ef38ac3f3b86be9c
Edsson Madrigal <akairu@hotmail.com>
2023-09-28 20:32:55 -0600

    Embeddings Advanced - Extract CSV amazon books

---
+++ utils/extract_from_csv.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+const path = require("path");
+
+//array where we will store the parse book data.
+let books = [];
+
+const booksFilePath = path.join(__dirname, "../parsed/books_data.csv");
+
+fs.createReadStream(booksFilePath)
+  // piping the read stream into the parse function
+  .pipe(
+    parse({
+      columns: [
+        "Title",
+        "description",
+        "authors",
+        "image",
+        "previewLink",
+        "publisher",
+        "publishedDate",
+        "infoLink",
+        "categories",
+        "ratingsCount",
+      ],
+      delimiter: ",",
+      quote: '"',
+      quote_empty: true,
+      quote_escape: '"',
+      relax_column_count: true,
+    })
+  )
+  .on("data", function (row) {
+    books.push(row);
+  })
+  .on("end", function () {
+    //convert books array to JSON format
+    const booksJSON = JSON.stringify(books);
+
+    //json filePath
+    const filePath = path.join(__dirname, "../parsed/books_data.json");
+
+    try {
+      fs.writeFileSync(filePath, booksJSON, "utf-8");
+    } catch (error) {
+      console.error("Error writing JSON file:", err);
+    }
+  });
